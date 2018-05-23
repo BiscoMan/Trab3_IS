42c92c14af9531bb413737fdfafd0f5915bb3f6b
BiscoMan <biscoman.contacto@gmail.com>
2018-05-24 00:25:56 +0100

    commit message

--- ElevatorsProject/src/ElevatorAgent/AchieveInitiator.java
+++ ElevatorsProject/src/ElevatorAgent/AchieveInitiator.java
@@ -6,6 +6,7 @@
 package ElevatorAgent;
 
 import jade.core.Agent;
+import jade.core.behaviours.WakerBehaviour;
 import jade.core.AID;
 import jade.lang.acl.ACLMessage;
 import jade.proto.AchieveREInitiator;
@@ -16,20 +17,50 @@ import Common.DFInteraction;
  *
  * @author gonca
  */
-public class AchieveInitiator extends Agent{
+public class AchieveInitiator extends Agent {
+
     DFAgentDescription[] dfd;
-    
+    AID name;
+    ACLMessage msg;
+
     @Override
-    protected void setup(){
-        AID name;
-        ACLMessage msg = new ACLMessage (ACLMessage.REQUEST);
+    protected void setup() {
+        msg = new ACLMessage(ACLMessage.REQUEST);
         dfd = DFInteraction.SearchInDF(this, "OA", "Elevator");
-        
-        //Fazer um waker behaviour para ver se há alguém
-        if(dfd.length != 0){
-            name = dfd[0].getName();
-            msg.addReceiver(name);
+        this.addBehaviour(new myBehaviour(this, 1000));
+        this.addBehaviour(new initiator(this, msg));
+    }
+
+    class myBehaviour extends WakerBehaviour {
+
+        public myBehaviour(Agent a, long timeout) {
+            super(a, timeout);
+        }
+
+        protected void onMake() {
+            if (dfd.length != 0) {
+                name = dfd[0].getName();
+                msg.addReceiver(name);
+                msg.setContent("ola");//perguntar como ir buscar o content
+            }
         }
     }
-    
+
+    class initiator extends AchieveREInitiator {
+
+        public initiator(Agent a, ACLMessage msg) {
+            super(a, msg);
+            send(msg);
+        }
+
+        @Override
+        protected void handleAgree(ACLMessage agree) {
+            
+        }
+
+        @Override
+        protected void handleInform(ACLMessage inform) {
+
+        }
+    }
 }
